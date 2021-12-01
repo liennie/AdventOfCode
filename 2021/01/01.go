3df4eabcb9937b7b2520f7ddd6a83c9cc67650f4
Trbola, Martin <martin.trbola@firma.seznam.cz>
2021-12-01 11:17:15 +0100

    Day 1 part 2

--- 2021/01/01.go
+++ 2021/01/01.go
@@ -6,16 +6,34 @@ import (
 	"github.com/liennie/AdventOfCode/common/util"
 )
 
-func countIncreases(filename string) int {
+func sum(i []int) int {
+	sum := 0
+	for _, n := range i {
+		sum += n
+	}
+	return sum
+}
+
+func countIncreases(filename string, window int) int {
 	count := 0
 
+	buf := make([]int, window)
+	i := 0
 	prev := int(^uint(0) >> 1)
 	for line := range load.File(filename) {
 		n := util.Atoi(line)
-		if n > prev {
-			count++
+		buf[i%window] = n
+
+		if i >= (window - 1) {
+			s := sum(buf)
+
+			if s > prev {
+				count++
+			}
+			prev = s
 		}
-		prev = n
+
+		i++
 	}
 	return count
 }
@@ -23,6 +41,11 @@ func countIncreases(filename string) int {
 func main() {
 	defer util.Recover(log.Err)
 
+	const filename = "input.txt"
+
 	// Part 1
-	log.Part1(countIncreases("input.txt"))
+	log.Part1(countIncreases(filename, 1))
+
+	// Part 2
+	log.Part2(countIncreases(filename, 3))
 }
