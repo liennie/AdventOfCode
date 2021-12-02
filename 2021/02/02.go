14d7b506ad2ff64e3dfa9bdcc40cf2d955393a19
Trbola, Martin <martin.trbola@firma.seznam.cz>
2021-12-02 10:32:32 +0100

    2021 day 2 part 2

--- 2021/02/02.go
+++ 2021/02/02.go
@@ -8,7 +8,7 @@ import (
 	"github.com/liennie/AdventOfCode/common/util"
 )
 
-func parse(filename string) (int, int) {
+func part1(filename string) (int, int) {
 	pos, depth := 0, 0
 
 	for line := range load.File(filename) {
@@ -27,13 +27,37 @@ func parse(filename string) (int, int) {
 	return pos, depth
 }
 
+func part2(filename string) (int, int) {
+	aim, pos, depth := 0, 0, 0
+
+	for line := range load.File(filename) {
+		switch {
+		case strings.HasPrefix(line, "forward"):
+			forward := util.Atoi(line[len("forward")+1:])
+			pos += forward
+			depth += aim * forward
+
+		case strings.HasPrefix(line, "down"):
+			aim += util.Atoi(line[len("down")+1:])
+
+		case strings.HasPrefix(line, "up"):
+			aim -= util.Atoi(line[len("up")+1:])
+		}
+	}
+
+	return pos, depth
+}
+
 func main() {
 	defer util.Recover(log.Err)
 
 	const filename = "input.txt"
 
-	pos, depth := parse(filename)
-
 	// Part 1
+	pos, depth := part1(filename)
 	log.Part1(pos * depth)
+
+	// Part 2
+	pos, depth = part2(filename)
+	log.Part2(pos * depth)
 }
