d3fe669a7826cb9cfb6908574827a43edf68c193
Trbola, Martin <martin.trbola@firma.seznam.cz>
2021-12-08 09:57:00 +0100

    2021 day 8 part 2

--- 2021/08/08.go
+++ 2021/08/08.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"sort"
 	"strings"
 
 	"github.com/liennie/AdventOfCode/common/load"
@@ -27,6 +28,87 @@ func parse(filename string) []entry {
 	return res
 }
 
+func sortSegments(s string) string {
+	b := []byte(s)
+	sort.Slice(b, func(i, j int) bool { return b[i] < b[j] })
+	return string(b)
+}
+
+func xorSegments(a, b string) string {
+	res := []rune{}
+	for r := 'a'; r <= 'g'; r++ {
+		if strings.ContainsRune(a, r) != strings.ContainsRune(b, r) {
+			res = append(res, r)
+		}
+	}
+	return string(res)
+}
+
+func doMagic(e entry) int {
+	digits := [10]string{}
+	fives := []string{}
+	sixes := []string{}
+
+	for _, pattern := range e.patterns {
+		switch len(pattern) {
+		case 2:
+			digits[1] = sortSegments(pattern)
+		case 3:
+			digits[7] = sortSegments(pattern)
+		case 4:
+			digits[4] = sortSegments(pattern)
+		case 7:
+			digits[8] = sortSegments(pattern)
+		case 5:
+			fives = append(fives, sortSegments(pattern))
+		case 6:
+			sixes = append(sixes, sortSegments(pattern))
+		}
+	}
+
+	for _, f := range fives {
+		if x := xorSegments(f, digits[7]); len(x) == 2 {
+			digits[3] = f
+		} else if x := xorSegments(f, digits[4]); len(x) == 5 {
+			digits[2] = f
+		} else {
+			digits[5] = f
+		}
+	}
+
+	for _, s := range sixes {
+		if x := xorSegments(s, digits[4]); len(x) == 2 {
+			digits[9] = s
+		} else if x := xorSegments(s, digits[1]); len(x) == 6 {
+			digits[6] = s
+		} else {
+			digits[0] = s
+		}
+	}
+
+	m := map[string]int{}
+	for i, pattern := range digits {
+		if len(pattern) == 0 {
+			util.Panic("We missed %d", i)
+		}
+		m[pattern] = i
+	}
+
+	res := 0
+
+	for _, digit := range e.digits {
+		d, ok := m[sortSegments(digit)]
+		if !ok {
+			util.Panic("Unknown digit %q, %+v", d, digits)
+		}
+
+		res *= 10
+		res += d
+	}
+
+	return res
+}
+
 func main() {
 	defer util.Recover(log.Err)
 
@@ -45,4 +127,11 @@ func main() {
 		}
 	}
 	log.Part1(count)
+
+	// Part 2
+	total := 0
+	for _, entry := range entries {
+		total += doMagic(entry)
+	}
+	log.Part2(total)
 }
