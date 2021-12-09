a4a3a0b22d3b6e713b76e39ddcd7cee392db492b
Trbola, Martin <martin.trbola@firma.seznam.cz>
2021-12-09 09:44:19 +0100

    2021 day 9 part 2

--- 2021/09/09.go
+++ 2021/09/09.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"sort"
 	"strings"
 
 	"github.com/liennie/AdventOfCode/common/load"
@@ -27,6 +28,7 @@ func main() {
 
 	// Part 1
 	low := []int{}
+	lowPoints := []util.Point{}
 	for i := range heightmap {
 		for j := range heightmap[i] {
 			c := heightmap[i][j]
@@ -36,8 +38,55 @@ func main() {
 				(i == len(heightmap)-1 || heightmap[i+1][j] > c) &&
 				(j == len(heightmap[i])-1 || heightmap[i][j+1] > c) {
 				low = append(low, c)
+				lowPoints = append(lowPoints, util.Point{
+					X: i,
+					Y: j,
+				})
 			}
 		}
 	}
 	log.Part1(util.Sum(low) + len(low))
+
+	// Part 2
+	basins := []int{}
+	for _, point := range lowPoints {
+		basin := map[util.Point]bool{}
+		stack := []util.Point{point}
+
+		for len(stack) > 0 {
+			cur := stack[len(stack)-1]
+			stack = stack[:len(stack)-1]
+
+			basin[cur] = true
+
+			if cur.X > 0 && heightmap[cur.X-1][cur.Y] < 9 {
+				new := util.Point{X: cur.X - 1, Y: cur.Y}
+				if !basin[new] {
+					stack = append(stack, new)
+				}
+			}
+			if cur.Y > 0 && heightmap[cur.X][cur.Y-1] < 9 {
+				new := util.Point{X: cur.X, Y: cur.Y - 1}
+				if !basin[new] {
+					stack = append(stack, new)
+				}
+			}
+			if cur.X < len(heightmap)-1 && heightmap[cur.X+1][cur.Y] < 9 {
+				new := util.Point{X: cur.X + 1, Y: cur.Y}
+				if !basin[new] {
+					stack = append(stack, new)
+				}
+			}
+			if cur.Y < len(heightmap[cur.X])-1 && heightmap[cur.X][cur.Y+1] < 9 {
+				new := util.Point{X: cur.X, Y: cur.Y + 1}
+				if !basin[new] {
+					stack = append(stack, new)
+				}
+			}
+		}
+
+		basins = append(basins, len(basin))
+	}
+	sort.Sort(sort.Reverse(sort.IntSlice(basins)))
+	log.Part2(basins[0] * basins[1] * basins[2])
 }
