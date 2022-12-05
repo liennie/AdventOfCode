c17f9e5c1127656a97c0f55c1a8fe81c02afe4df
Liennie <trbola.martin@seznam.cz>
2022-12-05 11:19:35 +0100

    2022 day 3 part 1

---
+++ 2022/03/03.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"strings"
+
+	"github.com/liennie/AdventOfCode/common/load"
+	"github.com/liennie/AdventOfCode/common/log"
+	"github.com/liennie/AdventOfCode/common/set"
+	"github.com/liennie/AdventOfCode/common/util"
+)
+
+type rucksack struct {
+	compartments []set.String
+}
+
+func parse(filename string) []rucksack {
+	res := []rucksack{}
+	for line := range load.File(filename) {
+		items := strings.Split(line, "")
+		res = append(res, rucksack{
+			compartments: []set.String{
+				set.New(items[:len(line)/2]...),
+				set.New(items[len(line)/2:]...),
+			},
+		})
+	}
+	return res
+}
+
+func priority(item string) int {
+	if len(item) != 1 {
+		util.Panic("Invalid item len %d", len(item))
+	}
+
+	c := item[0]
+	if c >= 'a' && c <= 'z' {
+		return int(c-'a') + 1
+	} else if c >= 'A' && c <= 'Z' {
+		return int(c-'A') + 27
+	}
+
+	util.Panic("Invalid item %c", c)
+	return 0
+}
+
+func main() {
+	defer util.Recover(log.Err)
+
+	const filename = "input.txt"
+
+	rucksacks := parse(filename)
+
+	// Part 1
+	sum := 0
+	for _, rucksack := range rucksacks {
+		for item := range set.Intersection(rucksack.compartments[0], rucksack.compartments[1]) {
+			sum += priority(item)
+		}
+	}
+	log.Part1(sum)
+}
--- common/set/set.go
+++ common/set/set.go
@@ -4,6 +4,12 @@ type Set[T comparable] map[T]struct{}
 
 type String = Set[string]
 
+func New[T comparable](items ...T) Set[T] {
+	set := Set[T]{}
+	set.Add(items...)
+	return set
+}
+
 func (s Set[T]) Add(items ...T) {
 	for _, item := range items {
 		s[item] = struct{}{}
@@ -14,4 +20,27 @@ func (s Set[T]) Remove(items ...T) {
 	for _, item := range items {
 		delete(s, item)
 	}
+}
+
+func (s Set[T]) Contains(items ...T) bool {
+	for _, item := range items {
+		if _, ok := s[item]; !ok {
+			return false
+		}
+	}
+	return true
+}
+
+func Intersection[T comparable](a, b Set[T]) Set[T] {
+	if len(b) < len(a) {
+		a, b = b, a
+	}
+
+	res := Set[T]{}
+	for item := range a {
+		if b.Contains(item) {
+			res.Add(item)
+		}
+	}
+	return res
 }
