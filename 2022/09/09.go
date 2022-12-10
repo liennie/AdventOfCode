6747b8cf7ab3d67ab1577ec7ff3d4d5ff8c49b00
Liennie <trbola.martin@seznam.cz>
2022-12-10 22:18:40 +0100

    2022 day 9 part 2

--- 2022/09/09.go
+++ 2022/09/09.go
@@ -35,6 +35,20 @@ func parse(filename string) []motion {
 	return res
 }
 
+func move(knots []space.Point, dir space.Point) {
+	knots[0] = knots[0].Add(dir)
+
+	for i := 1; i < len(knots); i++ {
+		v := knots[i-1].Sub(knots[i])
+		if ints.Abs(v.X) >= 2 || ints.Abs(v.Y) >= 2 {
+			knots[i] = knots[i].Add(space.Point{
+				X: ints.Clamp(v.X, -1, 1),
+				Y: ints.Clamp(v.Y, -1, 1),
+			})
+		}
+	}
+}
+
 func main() {
 	defer evil.Recover(log.Err)
 	filename := load.Filename()
@@ -42,22 +56,24 @@ func main() {
 	motions := parse(filename)
 
 	// Part 1
-	var head, tail space.Point
-	visited := set.New(tail)
+	knots := make([]space.Point, 2)
+	visited := set.New(space.Point{})
 	for _, motion := range motions {
 		for i := 0; i < motion.dist; i++ {
-			head = head.Add(motion.dir)
-
-			v := head.Sub(tail)
-			if ints.Abs(v.X) >= 2 || ints.Abs(v.Y) >= 2 {
-				tail = tail.Add(space.Point{
-					X: ints.Clamp(v.X, -1, 1),
-					Y: ints.Clamp(v.Y, -1, 1),
-				})
-
-				visited.Add(tail)
-			}
+			move(knots, motion.dir)
+			visited.Add(knots[1])
 		}
 	}
 	log.Part1(len(visited))
+
+	// Part 2
+	knots = make([]space.Point, 10)
+	visited = set.New(space.Point{})
+	for _, motion := range motions {
+		for i := 0; i < motion.dist; i++ {
+			move(knots, motion.dir)
+			visited.Add(knots[9])
+		}
+	}
+	log.Part2(len(visited))
 }
