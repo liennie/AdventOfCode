1ce3b4fdaf6967acb044daf2622217cb8394649e
Liennie <trbola.martin@seznam.cz>
2022-12-15 11:05:24 +0100

    2022 day 14 part 1

---
+++ 2022/14/14.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"math"
+	"strings"
+
+	"github.com/liennie/AdventOfCode/pkg/evil"
+	"github.com/liennie/AdventOfCode/pkg/ints"
+	"github.com/liennie/AdventOfCode/pkg/load"
+	"github.com/liennie/AdventOfCode/pkg/log"
+	"github.com/liennie/AdventOfCode/pkg/space"
+)
+
+type object uint8
+
+const (
+	air object = iota
+	rock
+	sand
+)
+
+func parse(filename string) map[space.Point]object {
+	res := map[space.Point]object{}
+	for line := range load.File(filename) {
+		path := []space.Point{}
+		for _, point := range strings.Split(line, " -> ") {
+			coords := evil.Split(point, ",")
+			path = append(path, space.Point{X: coords[0], Y: coords[1]})
+		}
+
+		for i := 1; i < len(path); i++ {
+			v := path[i].Sub(path[i-1]).Normalize()
+
+			for p := path[i-1]; !p.Equals(path[i]); p = p.Add(v) {
+				res[p] = rock
+			}
+			res[path[i]] = rock
+		}
+	}
+	return res
+}
+
+func main() {
+	defer evil.Recover(log.Err)
+	filename := load.Filename()
+
+	cave := parse(filename)
+
+	// Part 1
+	bottom := math.MinInt
+	for p := range cave {
+		bottom = ints.Max(bottom, p.Y)
+	}
+
+	count := 0
+sand:
+	for {
+		p := space.Point{X: 500, Y: 0}
+		for {
+			if np := p.Add(space.Point{Y: 1}); cave[np] == air {
+				p = np
+			} else if np := p.Add(space.Point{X: -1, Y: 1}); cave[np] == air {
+				p = np
+			} else if np := p.Add(space.Point{X: 1, Y: 1}); cave[np] == air {
+				p = np
+			} else {
+				cave[p] = sand
+				count++
+				break
+			}
+
+			if p.Y > bottom {
+				break sand
+			}
+		}
+	}
+	log.Part1(count)
+}
