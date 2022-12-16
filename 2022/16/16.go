5899889d1b363dbd4bca49e48f4a95d79b707123
Liennie <trbola.martin@seznam.cz>
2022-12-16 15:02:05 +0100

    2022 day 16 part 2

--- 2022/16/16.go
+++ 2022/16/16.go
@@ -47,31 +47,102 @@ func graph(valves map[string]*valve) path.Graph[string] {
 	})
 }
 
+type distKey struct {
+	from, to string
+}
+
+var dists = map[distKey]int{}
+
+func calculateDistances(valves map[string]*valve) {
+	g := graph(valves)
+
+	for from := range valves {
+		for to := range valves {
+			_, dist, err := path.Shortest(g, from, path.EndConst(to))
+			if err != nil {
+				evil.Panic("path error: %w", err)
+			}
+
+			dists[distKey{from: from, to: to}] = dist
+		}
+	}
+}
+
 func maximize(valves map[string]*valve, time int, cur string, closed set.String) int {
 	max := 0
 	nextClosed := closed.Clone()
 	for c := range closed {
-		if time == 30 {
-			log.Println(c)
-		}
+		nextClosed.Remove(c)
 
-		_, dist, err := path.Shortest(graph(valves), cur, path.EndConst(c))
-		if err != nil {
-			evil.Panic("path error: %w", err)
-		}
+		dist := dists[distKey{from: cur, to: c}]
 
 		rem := time - dist - 1
 		if rem < 0 {
 			continue
 		}
 
-		nextClosed.Remove(c)
 		gain := (rem * valves[c].rate) + maximize(valves, rem, c, nextClosed)
-		nextClosed.Add(c)
 
 		if gain > max {
 			max = gain
 		}
+
+		nextClosed.Add(c)
+	}
+	return max
+}
+
+func maximize2(valves map[string]*valve, time1, time2 int, cur1, cur2 string, closed set.String) int {
+	max := 0
+	nextClosed := closed.Clone()
+	for c1 := range closed {
+		nextClosed.Remove(c1)
+
+		if time1 == 26 {
+			log.Println(c1)
+		}
+
+		dist1 := dists[distKey{from: cur1, to: c1}]
+
+		rem1 := time1 - dist1 - 1
+		if rem1 < 0 {
+			continue
+		}
+
+		gain := (rem1 * valves[c1].rate) + maximize2(valves, rem1, time2, c1, cur2, nextClosed)
+
+		if gain > max {
+			max = gain
+		}
+
+		for c2 := range closed {
+			if c2 == c1 {
+				continue
+			}
+
+			if time1 == 26 && time2 == 26 {
+				log.Println(c1, c2)
+			}
+
+			nextClosed.Remove(c2)
+
+			dist2 := dists[distKey{from: cur2, to: c2}]
+
+			rem2 := time2 - dist2 - 1
+			if rem2 < 0 {
+				continue
+			}
+
+			gain := (rem1 * valves[c1].rate) + (rem2 * valves[c2].rate) + maximize2(valves, rem1, rem2, c1, c2, nextClosed)
+
+			if gain > max {
+				max = gain
+			}
+
+			nextClosed.Add(c2)
+		}
+
+		nextClosed.Add(c1)
 	}
 	return max
 }
@@ -81,6 +152,7 @@ func main() {
 	filename := load.Filename()
 
 	valves := parse(filename)
+	calculateDistances(valves)
 
 	// Part 1
 	closed := set.String{}
@@ -90,4 +162,7 @@ func main() {
 		}
 	}
 	log.Part1(maximize(valves, 30, "AA", closed))
+
+	// Part 2
+	log.Part2(maximize2(valves, 26, 26, "AA", "AA", closed))
 }
