980a5ea3e5f9cbfa24b48a7a37369e60351a4730
Liennie <trbola.martin@seznam.cz>
2023-12-01 11:07:31 +0100

    2023 day 1 part 2

--- 2023/01/01.go
+++ 2023/01/01.go
@@ -1,10 +1,34 @@
 package main
 
 import (
+	"strings"
+
 	"github.com/liennie/AdventOfCode/pkg/evil"
 	"github.com/liennie/AdventOfCode/pkg/load"
 	"github.com/liennie/AdventOfCode/pkg/log"
 )
+
+var validDigits = map[string]int{
+	"0":     0,
+	"1":     1,
+	"2":     2,
+	"3":     3,
+	"4":     4,
+	"5":     5,
+	"6":     6,
+	"7":     7,
+	"8":     8,
+	"9":     9,
+	"one":   1,
+	"two":   2,
+	"three": 3,
+	"four":  4,
+	"five":  5,
+	"six":   6,
+	"seven": 7,
+	"eight": 8,
+	"nine":  9,
+}
 
 func main() {
 	defer evil.Recover(log.Err)
@@ -34,4 +58,25 @@ func main() {
 		sum += first*10 + last
 	}
 	log.Part1(sum)
+
+	// Part 2
+	sum = 0
+	for _, line := range lines {
+		first, last := 0, 0
+		firstIndex, lastIndex := len(line), -1
+
+		for digit, value := range validDigits {
+			if index := strings.Index(line, digit); index >= 0 && index < firstIndex {
+				firstIndex = index
+				first = value
+			}
+			if index := strings.LastIndex(line, digit); index >= 0 && index > lastIndex {
+				lastIndex = index
+				last = value
+			}
+		}
+
+		sum += first*10 + last
+	}
+	log.Part2(sum)
 }
