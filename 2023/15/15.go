10bbf4452d2cd53b9623f20adf94e8921a60ec64
Liennie <trbola.martin@seznam.cz>
2023-12-15 10:30:55 +0100

    2023 day 15 part 2

--- 2023/15/15.go
+++ 2023/15/15.go
@@ -1,12 +1,18 @@
 package main
 
 import (
+	"slices"
 	"strings"
 
 	"github.com/liennie/AdventOfCode/pkg/evil"
 	"github.com/liennie/AdventOfCode/pkg/load"
 	"github.com/liennie/AdventOfCode/pkg/log"
 )
+
+type Lens struct {
+	label string
+	focal int
+}
 
 func parse(filename string) []string {
 	res := []string{}
@@ -39,4 +45,33 @@ func main() {
 		sum += hash(s)
 	}
 	log.Part1(sum)
+
+	// Part 2
+	boxes := [256][]Lens{}
+	for _, s := range seq {
+		if label, ok := strings.CutSuffix(s, "-"); ok {
+			i := hash(label)
+			boxes[i] = slices.DeleteFunc(boxes[i], func(l Lens) bool { return l.label == label })
+		} else if label, n, ok := strings.Cut(s, "="); ok {
+			i := hash(label)
+			if j := slices.IndexFunc(boxes[i], func(l Lens) bool { return l.label == label }); j >= 0 {
+				boxes[i][j].focal = evil.Atoi(n)
+			} else {
+				boxes[i] = append(boxes[i], Lens{
+					label: label,
+					focal: evil.Atoi(n),
+				})
+			}
+		} else {
+			evil.Panic("invalid step %q", s)
+		}
+	}
+
+	sum = 0
+	for i, box := range boxes {
+		for j, lens := range box {
+			sum += (i + 1) * (j + 1) * lens.focal
+		}
+	}
+	log.Part2(sum)
 }
