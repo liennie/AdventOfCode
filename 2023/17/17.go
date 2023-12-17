27bca097fa0f8291500b8c3b972fbe338b59680d
Liennie <trbola.martin@seznam.cz>
2023-12-17 11:38:31 +0100

    2023 day 17 part 2

--- 2023/17/17.go
+++ 2023/17/17.go
@@ -22,6 +22,58 @@ type Node struct {
 	pos  space.Point
 }
 
+type Graph struct {
+	blocks   [][]int
+	aabb     space.AABB
+	min, max int
+}
+
+func (g Graph) Edges(n Node) []path.Edge[Node] {
+	edges := []path.Edge[Node]{}
+
+	for _, dir := range []space.Point{{X: 1}, {Y: -1}, {X: -1}, {Y: 1}} {
+		if n.from.Normalize().Flip() == dir {
+			continue
+		}
+
+		cost := 0
+		for i := 1; i <= g.max; i++ {
+			tdir := dir.Scale(i)
+			npos := n.pos.Add(tdir)
+			if !g.aabb.Contains(npos) {
+				break
+			}
+
+			tfrom := n.from.Add(tdir)
+			if ints.Abs(tfrom.X) > g.max || ints.Abs(tfrom.Y) > g.max {
+				break
+			}
+
+			cost += g.blocks[npos.Y][npos.X]
+			if i < g.min {
+				continue
+			}
+
+			var from space.Point
+			if n.from.Normalize() == dir {
+				from = tfrom
+			} else {
+				from = tdir
+			}
+
+			edges = append(edges, path.Edge[Node]{
+				Len: cost,
+				To: Node{
+					from: from,
+					pos:  npos,
+				},
+			})
+		}
+	}
+
+	return edges
+}
+
 func main() {
 	defer evil.Recover(log.Err)
 	filename := load.Filename()
@@ -34,47 +86,30 @@ func main() {
 	aabb := space.NewAABB(space.Point{}, end)
 
 	// Part 1
-	_, l, err := path.Shortest(path.GraphFunc[Node](func(n Node) []path.Edge[Node] {
-		edges := []path.Edge[Node]{}
-
-		for _, dir := range []space.Point{{X: 1}, {Y: -1}, {X: -1}, {Y: 1}} {
-			if n.from.Normalize().Flip() == dir {
-				continue
-			}
-
-			cost := 0
-			for i := 1; i <= 3; i++ {
-				tdir := dir.Scale(i)
-				npos := n.pos.Add(tdir)
-				if !aabb.Contains(npos) {
-					break
-				}
-
-				tfrom := n.from.Add(tdir)
-				if ints.Abs(tfrom.X) > 3 || ints.Abs(tfrom.Y) > 3 {
-					break
-				}
-
-				cost += blocks[npos.Y][npos.X]
-				var from space.Point
-				if n.from.Normalize() == dir {
-					from = tfrom
-				} else {
-					from = tdir
-				}
-
-				edges = append(edges, path.Edge[Node]{
-					Len: cost,
-					To: Node{
-						from: from,
-						pos:  npos,
-					},
-				})
-			}
-		}
-
-		return edges
-	}), Node{pos: space.Point{X: 0, Y: 0}}, path.EndFunc[Node](func(n Node) bool { return n.pos == end }))
+	_, l, err := path.Shortest(
+		Graph{
+			blocks: blocks,
+			aabb:   aabb,
+			min:    1,
+			max:    3,
+		},
+		Node{pos: space.Point{X: 0, Y: 0}},
+		path.EndFunc[Node](func(n Node) bool { return n.pos == end }),
+	)
 	evil.Assert(err == nil, err)
 	log.Part1(l)
+
+	// Part 2
+	_, l, err = path.Shortest(
+		Graph{
+			blocks: blocks,
+			aabb:   aabb,
+			min:    4,
+			max:    10,
+		},
+		Node{pos: space.Point{X: 0, Y: 0}},
+		path.EndFunc[Node](func(n Node) bool { return n.pos == end }),
+	)
+	evil.Assert(err == nil, err)
+	log.Part2(l)
 }
