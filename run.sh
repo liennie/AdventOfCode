b5935c12d9d27b0e3c0b2533770a2eda38d55eef
liennie <trbola.martin@seznam.cz>
2024-12-01 19:54:36 +0100

    Prepare in run.sh

--- run.sh
+++ run.sh
@@ -3,6 +3,17 @@
 
 YEAR=$(TZ=EST date +%Y)
 DAY=$(TZ=EST date +%d)
+DIR="$YEAR/$DAY"
+PREP=bin/prepare
+INPUT=input.txt
+
+if [ ! -f "$DIR/$INPUT" ]; then
+	if [ ! -x "$PREP" ]; then
+		make "$PREP"
+	fi
+
+	"$PREP"
+fi
 
 cd "$YEAR/$DAY"
-exec go run $DAY.go "$@"
+exec go run "$DAY.go" "$@"
