6b07b571d37f66913a6f7e9463ec70d84affedf0
liennie <trbola.martin@seznam.cz>
2024-12-02 14:15:19 +0100

    2024 day 2 part 2

--- 2024/02/02.go
+++ 2024/02/02.go
@@ -15,6 +15,27 @@ func parse(filename string) [][]int {
 	return res
 }
 
+func isSafe(report []int) bool {
+	increasing := report[0] < report[1]
+	for i := range len(report) - 1 {
+		a, b := report[i], report[i+1]
+		if diff := ints.Abs(a - b); 1 > diff || diff > 3 {
+			return false
+		}
+		if increasing && a > b {
+			return false
+		}
+		if !increasing && a < b {
+			return false
+		}
+	}
+	return true
+}
+
+func remove(report []int, i int) []int {
+	return append(report[:i:i], report[i+1:]...)
+}
+
 func main() {
 	defer evil.Recover(log.Err)
 	filename := load.Filename()
@@ -23,24 +44,27 @@ func main() {
 
 	// Part 1
 	safe := 0
-reports:
 	for r, report := range reports {
 		evil.Assert(len(report) > 1, "report ", r, " has len 1")
+		if isSafe(report) {
+			safe++
+		}
+	}
+	log.Part1(safe)
 
-		increasing := report[0] < report[1]
-		for i := range len(report) - 1 {
-			a, b := report[i], report[i+1]
-			if diff := ints.Abs(a - b); 1 > diff || diff > 3 {
-				continue reports
-			}
-			if increasing && a > b {
-				continue reports
-			}
-			if !increasing && a < b {
-				continue reports
+	// Part 2
+	safe = 0
+	for _, report := range reports {
+		if isSafe(report) {
+			safe++
+		} else {
+			for i := range report {
+				if isSafe(remove(report, i)) {
+					safe++
+					break
+				}
 			}
 		}
-		safe++
 	}
 	log.Part1(safe)
 }
