9bebe18c23fc2223e4dbe129cd05e1393bf362a3
liennie <trbola.martin@seznam.cz>
2024-12-04 17:31:51 +0100

    2024 day 4 part 1

---
+++ 2024/04/04.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"strings"
+
+	"github.com/liennie/AdventOfCode/pkg/evil"
+	"github.com/liennie/AdventOfCode/pkg/load"
+	"github.com/liennie/AdventOfCode/pkg/log"
+	"github.com/liennie/AdventOfCode/pkg/space"
+)
+
+func parse(filename string) map[space.Point]string {
+	res := map[space.Point]string{}
+	y := 0
+	for line := range load.File(filename) {
+		for x, letter := range strings.Split(line, "") {
+			res[space.Point{x, y}] = letter
+		}
+		y++
+	}
+	return res
+}
+
+func main() {
+	defer evil.Recover(log.Err)
+	filename := load.Filename()
+
+	grid := parse(filename)
+
+	// Part 1
+	dirs := []space.Point{
+		{1, 0},
+		{1, 1},
+		{0, 1},
+		{-1, 1},
+		{-1, 0},
+		{-1, -1},
+		{0, -1},
+		{1, -1},
+	}
+	word := strings.Split("XMAS", "")
+
+	count := 0
+	for start := range grid {
+	dirs:
+		for _, dir := range dirs {
+			for n, letter := range word {
+				if grid[start.Add(dir.Scale(n))] != letter {
+					continue dirs
+				}
+			}
+
+			count++
+		}
+	}
+	log.Part1(count)
+}
