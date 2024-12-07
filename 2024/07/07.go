b6ba14aaa4d18dd0ee3e66e6e367b2c549e17f25
liennie <trbola.martin@seznam.cz>
2024-12-07 16:52:18 +0100

    2024 day 7 part 2

--- 2024/07/07.go
+++ 2024/07/07.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"strconv"
 	"strings"
 
 	"github.com/liennie/AdventOfCode/pkg/evil"
@@ -27,15 +28,30 @@ func parse(filename string) []Equation {
 	})
 }
 
-func isPossible(target, a, b int, rest ...int) bool {
-	if len(rest) == 0 {
-		return a*b == target || a+b == target
+type Eval struct {
+	operators []func(int, int) int
+}
+
+func (e Eval) isPossible(target, a, b int, rest ...int) bool {
+	if a > target {
+		return false
 	}
 
-	if isPossible(target, a*b, rest[0], rest[1:]...) {
-		return true
+	if len(rest) == 0 {
+		for _, op := range e.operators {
+			if op(a, b) == target {
+				return true
+			}
+		}
+		return false
 	}
-	return isPossible(target, a+b, rest[0], rest[1:]...)
+
+	for _, op := range e.operators {
+		if e.isPossible(target, op(a, b), rest[0], rest[1:]...) {
+			return true
+		}
+	}
+	return false
 }
 
 func main() {
@@ -45,11 +61,33 @@ func main() {
 	equations := parse(filename)
 
 	// Part 1
+	e := Eval{
+		operators: []func(int, int) int{
+			func(a, b int) int { return a * b },
+			func(a, b int) int { return a + b },
+		},
+	}
 	sum := 0
 	for _, eq := range equations {
-		if isPossible(eq.target, eq.values[0], eq.values[1], eq.values[2:]...) {
+		if e.isPossible(eq.target, eq.values[0], eq.values[1], eq.values[2:]...) {
 			sum += eq.target
 		}
 	}
 	log.Part1(sum)
+
+	// Part 2
+	e = Eval{
+		operators: []func(int, int) int{
+			func(a, b int) int { return a * b },
+			func(a, b int) int { return a + b },
+			func(a, b int) int { return evil.Atoi(strconv.Itoa(a) + strconv.Itoa(b)) },
+		},
+	}
+	sum = 0
+	for _, eq := range equations {
+		if e.isPossible(eq.target, eq.values[0], eq.values[1], eq.values[2:]...) {
+			sum += eq.target
+		}
+	}
+	log.Part2(sum)
 }
