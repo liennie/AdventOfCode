30c7c4eeae4cc1d718ef6e0efa2956b20ba96fdb
liennie <trbola.martin@seznam.cz>
2024-12-08 15:01:20 +0100

    2024 day 8 part 1

---
+++ 2024/08/08.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"github.com/liennie/AdventOfCode/pkg/evil"
+	"github.com/liennie/AdventOfCode/pkg/load"
+	"github.com/liennie/AdventOfCode/pkg/log"
+	"github.com/liennie/AdventOfCode/pkg/set"
+	"github.com/liennie/AdventOfCode/pkg/space"
+)
+
+func parse(filename string) (map[rune][]space.Point, space.AABB) {
+	res := map[rune][]space.Point{}
+	aabb := space.AABB{}
+	load.Grid(filename, func(x, y int, r rune) {
+		aabb = aabb.Add(space.Point{x, y})
+		if ('a' <= r && r <= 'z') || ('A' <= r && r <= 'Z') || ('0' <= r && r <= '9') {
+			res[r] = append(res[r], space.Point{x, y})
+		}
+	})
+	return res, aabb
+}
+
+func main() {
+	defer evil.Recover(log.Err)
+	filename := load.Filename()
+
+	antennas, aabb := parse(filename)
+
+	// Part 1
+	antinodes := set.New[space.Point]()
+	for _, ant := range antennas {
+		for i := range len(ant) - 1 {
+			for j := i + 1; j < len(ant); j++ {
+				a, b := ant[i], ant[j]
+				d := b.Sub(a)
+
+				antiA := a.Sub(d)
+				if aabb.Contains(antiA) {
+					antinodes.Add(antiA)
+				}
+
+				antiB := b.Add(d)
+				if aabb.Contains(antiB) {
+					antinodes.Add(antiB)
+				}
+			}
+		}
+	}
+	log.Part1(len(antinodes))
+}
