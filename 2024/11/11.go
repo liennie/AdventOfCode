18d3ebb4a416ce2b3db7d6bd3a98c6fa107f1f4e
liennie <trbola.martin@seznam.cz>
2024-12-11 10:04:28 +0100

    2024 day 11 part 2

--- 2024/11/11.go
+++ 2024/11/11.go
@@ -13,21 +13,41 @@ func parse(filename string) []int {
 	return evil.Fields(load.Line(filename))
 }
 
-func blink(stones []int) []int {
-	for i := len(stones) - 1; i >= 0; i-- {
-		if stones[i] == 0 {
-			stones[i] = 1
-		} else if s := strconv.Itoa(stones[i]); len(s)%2 == 0 {
-			p := ints.Pow(10, len(s)/2)
+var blinkRuleBuf = make([]int, 2)
 
-			// in part 1 order doesn't matter
-			stones = append(stones, stones[i]%p)
-			stones[i] /= p
-		} else {
-			stones[i] *= 2024
+// uses global buffer, cannot be called concurrently
+func blinkRule(stone int) []int {
+	if stone == 0 {
+		blinkRuleBuf[0] = 1
+		return blinkRuleBuf[:1]
+	}
+
+	if s := strconv.Itoa(stone); len(s)%2 == 0 {
+		p := ints.Pow(10, len(s)/2)
+
+		blinkRuleBuf[0] = stone / p
+		blinkRuleBuf[1] = stone % p
+		return blinkRuleBuf
+	}
+
+	blinkRuleBuf[0] = stone * 2024
+	return blinkRuleBuf[:1]
+}
+
+func blink(stones map[int]int, res map[int]int) {
+	for n, count := range stones {
+		for _, m := range blinkRule(n) {
+			res[m] += count
 		}
 	}
-	return stones
+}
+
+func sum(stones map[int]int) int {
+	s := 0
+	for _, count := range stones {
+		s += count
+	}
+	return s
 }
 
 func main() {
@@ -36,10 +56,28 @@ func main() {
 
 	stones := parse(filename)
 
+	stoneMap := map[int]int{}
+	for _, stone := range stones {
+		stoneMap[stone]++
+	}
+
+	resMap := map[int]int{}
+
 	// Part 1
 	blinks := 25
 	for range blinks {
-		stones = blink(stones)
+		blink(stoneMap, resMap)
+		stoneMap, resMap = resMap, stoneMap
+		clear(resMap)
 	}
-	log.Part1(len(stones))
+	log.Part1(sum(stoneMap))
+
+	// Part 2
+	blinks = 50 // 25 + 50 = 75 blinks total
+	for range blinks {
+		blink(stoneMap, resMap)
+		stoneMap, resMap = resMap, stoneMap
+		clear(resMap)
+	}
+	log.Part2(sum(stoneMap))
 }
