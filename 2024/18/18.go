57701c3bcecf20952822c627f11a9dc117a68c99
liennie <trbola.martin@seznam.cz>
2024-12-18 14:25:18 +0100

    2024 day 18 part 2

--- 2024/18/18.go
+++ 2024/18/18.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"errors"
+	"fmt"
+
 	"github.com/liennie/AdventOfCode/pkg/evil"
 	"github.com/liennie/AdventOfCode/pkg/load"
 	"github.com/liennie/AdventOfCode/pkg/log"
@@ -16,6 +19,21 @@ func parse(filename string) (res []space.Point) {
 		res = append(res, space.Point{n[0], n[1]})
 	}
 	return
+}
+
+func graph(aabb space.AABB, corrupted set.Set[space.Point]) path.GraphFunc[space.Point] {
+	return func(p space.Point) (edges []path.Edge[space.Point]) {
+		for dir := range space.Orthogonal() {
+			next := p.Add(dir)
+			if aabb.Contains(next) && !corrupted.Contains(next) {
+				edges = append(edges, path.Edge[space.Point]{
+					Len: 1,
+					To:  next,
+				})
+			}
+		}
+		return
+	}
 }
 
 func main() {
@@ -35,19 +53,18 @@ func main() {
 	}
 
 	corrupted := set.New(bytes[:fallen]...)
-	_, steps, err := path.Shortest(path.GraphFunc[space.Point](func(p space.Point) (edges []path.Edge[space.Point]) {
-		for dir := range space.Orthogonal() {
-			next := p.Add(dir)
-			if aabb.Contains(next) && !corrupted.Contains(next) {
-				edges = append(edges, path.Edge[space.Point]{
-					Len: 1,
-					To:  next,
-				})
-			}
-		}
-		return
-	}), aabb.Min, path.EndConst(aabb.Max))
+	_, steps, err := path.Shortest(graph(aabb, corrupted), aabb.Min, path.EndConst(aabb.Max))
 	evil.Assert(err == nil, "path not found: ", err)
 
 	log.Part1(steps)
+
+	// Part 2
+	for _, falling := range bytes[fallen:] {
+		corrupted.Add(falling)
+		_, _, err := path.Shortest(graph(aabb, corrupted), aabb.Min, path.EndConst(aabb.Max))
+		if errors.Is(err, path.ErrNotFound) {
+			log.Part2(fmt.Sprintf("%d,%d", falling.X, falling.Y))
+			break
+		}
+	}
 }
