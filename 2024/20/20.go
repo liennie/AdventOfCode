87b7c2f48bfdb8a26d3ccec591ccb5e46e2ec9bf
liennie <trbola.martin@seznam.cz>
2024-12-20 11:43:46 +0100

    2024 day 20 part 2

--- 2024/20/20.go
+++ 2024/20/20.go
@@ -5,6 +5,7 @@ import (
 	"github.com/liennie/AdventOfCode/pkg/load"
 	"github.com/liennie/AdventOfCode/pkg/log"
 	"github.com/liennie/AdventOfCode/pkg/path"
+	"github.com/liennie/AdventOfCode/pkg/set"
 	"github.com/liennie/AdventOfCode/pkg/space"
 )
 
@@ -25,13 +26,64 @@ func parse(filename string) (map[space.Point]rune, space.Point, space.Point) {
 	return res, start, end
 }
 
+func cheatDests(track map[space.Point]rune, start space.Point, max int) map[space.Point]int {
+	res := map[space.Point]int{}
+	visited := set.New[space.Point]()
+	cur := set.New(start)
+
+	for dist := 1; dist <= max; dist++ {
+		visited.AddSeq(cur.All())
+
+		next := set.New[space.Point]()
+		for p := range cur {
+			for dir := range space.Orthogonal() {
+				n := p.Add(dir)
+				if visited.Contains(n) {
+					continue
+				}
+
+				if track[n] == '.' {
+					if _, ok := res[n]; !ok {
+						res[n] = dist
+					}
+				}
+				next.Add(n)
+			}
+		}
+		cur = next
+	}
+
+	return res
+}
+
+func cheats(track map[space.Point]rune, path []space.Point, max int) map[int]int {
+	dist := map[space.Point]int{}
+	for i, p := range path {
+		dist[p] = i
+	}
+
+	cheats := map[int]int{}
+	for _, p := range path {
+		for next, d := range cheatDests(track, p, max) {
+			if dist[p] > dist[next] {
+				continue
+			}
+
+			cheat := dist[next] - dist[p] - d
+			if cheat > 0 {
+				cheats[cheat]++
+			}
+		}
+	}
+	return cheats
+}
+
 func main() {
 	defer evil.Recover(log.Err)
 	filename := load.Filename()
 
 	track, start, end := parse(filename)
 
-	// Part 1
 	path, _, err := path.Shortest(path.GraphFunc[space.Point](func(p space.Point) (edges []path.Edge[space.Point]) {
 		for dir := range space.Orthogonal() {
 			next := p.Add(dir)
@@ -46,30 +98,21 @@ func main() {
 	}), start, path.EndConst(end))
 	evil.Assert(err == nil, "no path")
 
-	dist := map[space.Point]int{}
-	for i, p := range path {
-		dist[p] = i
-	}
-
+	// Part 1
 	cheats100 := 0
-	for _, p := range path {
-		for dir := range space.Orthogonal() {
-			if track[p.Add(dir)] != '#' {
-				continue
-			}
-			next := p.Add(dir.Scale(2))
-			if track[next] != '.' {
-				continue
-			}
-			if dist[p] > dist[next] {
-				continue
-			}
-
-			cheat := dist[next] - dist[p] - 2
-			if cheat >= 100 {
-				cheats100++
-			}
+	for dist, cnt := range cheats(track, path, 2) {
+		if dist >= 100 {
+			cheats100 += cnt
 		}
 	}
 	log.Part1(cheats100)
+
+	// Part 2
+	cheats100 = 0
+	for dist, cnt := range cheats(track, path, 20) {
+		if dist >= 100 {
+			cheats100 += cnt
+		}
+	}
+	log.Part2(cheats100)
 }
