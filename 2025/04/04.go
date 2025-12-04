98d84399666472d818c73d4d03723bd567f5b088
liennie <trbola.martin@seznam.cz>
2025-12-04 10:32:41 +0100

    2025 day 4 part 2

--- 2025/04/04.go
+++ 2025/04/04.go
@@ -4,6 +4,7 @@ import (
 	"github.com/liennie/AdventOfCode/pkg/evil"
 	"github.com/liennie/AdventOfCode/pkg/load"
 	"github.com/liennie/AdventOfCode/pkg/log"
+	"github.com/liennie/AdventOfCode/pkg/set"
 	"github.com/liennie/AdventOfCode/pkg/space"
 )
 
@@ -13,6 +14,16 @@ func parse(filename string) map[space.Point]rune {
 		res[p] = r
 	})
 	return res
+}
+
+func isAccessible(papers map[space.Point]rune, p space.Point) bool {
+	adj := 0
+	for dir := range space.Neighbors() {
+		if papers[p.Add(dir)] == '@' {
+			adj++
+		}
+	}
+	return adj < 4
 }
 
 func main() {
@@ -28,15 +39,37 @@ func main() {
 			continue
 		}
 
-		adj := 0
-		for dir := range space.Neighbors() {
-			if papers[p.Add(dir)] == '@' {
-				adj++
-			}
-		}
-		if adj < 4 {
+		if isAccessible(papers, p) {
 			cnt++
 		}
 	}
 	log.Part1(cnt)
+
+	// Part 2
+	cnt = 0
+	cont := true
+	for cont {
+		cont = false
+
+		toRemove := set.New[space.Point]()
+		for p, r := range papers {
+			if r != '@' {
+				continue
+			}
+
+			if isAccessible(papers, p) {
+				toRemove.Add(p)
+			}
+		}
+
+		if len(toRemove) > 0 {
+			cnt += len(toRemove)
+			cont = true
+
+			for p := range toRemove {
+				papers[p] = '.'
+			}
+		}
+	}
+	log.Part2(cnt)
 }
